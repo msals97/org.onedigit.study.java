145bbe319498bddf7e6e56d3683ebc438532374d
RIZAA <RIZAA@C0155.daiwaeurope.local>
2012-10-25 16:16:05 +0100

    object size

--- src/main/java/org/onedigit/study/java/unsafe/UnsafeOps.java
+++ src/main/java/org/onedigit/study/java/unsafe/UnsafeOps.java
@@ -1,6 +1,9 @@
 package org.onedigit.study.java.unsafe;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.LinkedList;
+import java.util.List;
 
 import sun.misc.Unsafe;
 
@@ -8,18 +11,23 @@ import com.sun.corba.se.impl.ior.ByteBuffer;
 
 public class UnsafeOps extends ByteBuffer
 {
-	public UnsafeOps()
-	{
-		Unsafe unsafe = getUnsafe();
-		int addressSize = unsafe.addressSize();
-		System.out.println("Address size = " + addressSize);
-		System.out.println("Page size = " + unsafe.pageSize());
-		int nelems = 3;
-		double[] loadavg = new double[nelems];
-		System.out.println("Load avg = " + unsafe.getLoadAverage(loadavg, nelems));
+	private static Unsafe unsafe = getUnsafe();
+	private static final long parkBlockerOffset;
+	private static final int NR_BITS = Integer.valueOf(System.getProperty("sun.arch.data.model"));
+	private static final int BYTE = 8;
+	private static final int WORD = NR_BITS/BYTE;
+	private static final int MIN_SIZE = 16; 
+
+	static {
+		try {
+			parkBlockerOffset = unsafe.objectFieldOffset(
+					java.lang.Thread.class.getDeclaredField("parkBlocker"));
+		} catch (Exception ex) {
+			throw new Error(ex);
+		}
 	}
 
-	private Unsafe getUnsafe()
+	private static Unsafe getUnsafe()
 	{
 		String fieldName = "theUnsafe";
 		Unsafe unsafe = null;
@@ -33,9 +41,90 @@ public class UnsafeOps extends ByteBuffer
 		}
 		return unsafe;
 	}
+	
+	public static int sizeOf(Class<?> src)
+	{
+		//
+		// Get the instance fields of src class
+		//
+		List<Field> instanceFields = new LinkedList<Field>();
+		do {
+			if (src == Object.class)
+				return MIN_SIZE;
+			for (Field f : src.getDeclaredFields()) {
+				if ((f.getModifiers() & Modifier.STATIC) == 0) {
+					instanceFields.add(f);
+				}
+			}
+			src = src.getSuperclass();
+		} while (instanceFields.isEmpty());
+		//
+		// Get the field with the maximum offset
+		//
+		long maxOffset = 0;
+		for (Field f : instanceFields) {
+			long offset = unsafe.objectFieldOffset(f);
+			if (offset > maxOffset)
+				maxOffset = offset;
+		}
+		return (((int) maxOffset / WORD) + 1) * WORD;
+	}
+
+	private static void setBlocker(Thread t, Object arg)
+	{
+		// Even though volatile, hotspot doesn't need a write barrier here.
+		unsafe.putObject(t, parkBlockerOffset, arg);
+	}
+
+	public static void park(Object blocker)
+	{
+		Thread t = Thread.currentThread();
+		setBlocker(t, blocker);
+		unsafe.park(false, 0L);
+		setBlocker(t, null);
+	}
+	
+	private void sleep(int n)
+	{
+		try {
+            Thread.sleep(n);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }		
+	}
+
+	public void testThreadPark()
+	{
+		Thread thr = new Thread(new Runnable() {
+			public void run()
+			{
+				System.out.println("Run...");
+				park(this);
+			}
+		});
+
+		thr.start();
+		sleep(5_000);
+		System.out.println("Unparking thread...");
+		unsafe.unpark(thr);
+	}
+
+	public UnsafeOps()
+	{
+		int addressSize = unsafe.addressSize();
+		System.out.println("Address size = " + addressSize);
+		System.out.println("Page size = " + unsafe.pageSize());
+		int nelems = 3;
+		double[] loadavg = new double[nelems];
+		System.out.println("Load avg = "
+		        + unsafe.getLoadAverage(loadavg, nelems));
+	}
 
 	public static void main(String... args)
 	{
-		new UnsafeOps();
+		UnsafeOps ops = new UnsafeOps();
+		// ops.testThreadPark();
+		System.out.println(UnsafeOps.sizeOf(java.util.concurrent.ConcurrentHashMap.class));
+		System.out.println(UnsafeOps.sizeOf(java.util.HashMap.class));
 	}
 }
