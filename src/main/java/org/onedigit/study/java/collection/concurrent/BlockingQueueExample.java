965c8dbc15a301f555a7be9de2317955b6d3bd73
RIZAA <RIZAA@C0155.daiwaeurope.local>
2012-11-01 12:01:38 +0000

    blq

--- src/main/java/org/onedigit/study/java/collection/concurrent/BlockingQueueExample.java
+++ src/main/java/org/onedigit/study/java/collection/concurrent/BlockingQueueExample.java
@@ -61,15 +61,30 @@ public class BlockingQueueExample
 	PriorityBlockingQueue<Integer> pBQ; // implements BlockingQueue implements Queue 
 	SynchronousQueue<Integer> sQ; // implements BlockingQueue implements Queue 
 	
-	// implements BlockingQueue implements Queue
-	// implements Deque
+	// implements BlockingDeque implements BlockingQueue implements Queue
+	//							implements Deque
 	LinkedBlockingDeque<Integer> lBQ;
 	
 	// implements TransferQueue implements BlockingQueue implements Queue
 	LinkedTransferQueue<Integer> lTQ;
 	
-	ArrayDeque<Integer> aD; // implements Deque
+	ArrayDeque<Integer> aDQ; // implements Deque
 	LinkedList<Integer> linkedList; // implements Deque
+	
+	public void arrayDeque()
+	{
+		aDQ = new ArrayDeque<>();
+		int head = 25;
+		head = (head - 1) & (20 - 1);
+		System.out.println(head);
+		
+		aDQ.add(10);
+		aDQ.offer(20);
+		aDQ.addFirst(50);
+		aDQ.addFirst(40);
+		aDQ.addFirst(30);
+		System.out.println(aDQ);
+	}
 	
 	public void arrayBlockingQueue() throws InterruptedException
 	{
@@ -158,7 +173,8 @@ public class BlockingQueueExample
 	public static void main(String... args) throws InterruptedException
 	{
 		BlockingQueueExample example = new BlockingQueueExample();
-		example.arrayBlockingQueue();
+		// example.arrayBlockingQueue();
 		// example.delayQueue();
+		example.arrayDeque();
 	}
 }
